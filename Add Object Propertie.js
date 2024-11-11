a3f6883f1f732e7618e2e994816c85be3d70584e
sricloudshare360 <sri.cloudshare360@gmail.com>
2024-11-12 00:25:06 +0530

    Create Add Object Propertie.js

---
+++ Add Object Propertie.js
@@ -0,0 +1,13 @@
+const student = {
+    name: "John",
+    age: 20,
+};
+
+// add properties
+student.rollNo = 14;
+student.faculty = "Science";
+
+// display the object
+console.log(student);
+
+// Output: { name: 'John', age: 20, rollNo: 14, faculty: 'Science' }
