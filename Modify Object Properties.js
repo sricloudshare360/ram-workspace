b8dbb8a3d3883f62d08d232e5dbf81aadb750958
sricloudshare360 <sri.cloudshare360@gmail.com>
2024-11-12 00:08:29 +0530

    Create Modify Object Properties.js

---
+++ Modify Object Properties.js
@@ -0,0 +1,12 @@
+const person = {
+    name: "Bobby",
+    hobby: "Dancing",
+};
+
+// modify property
+person.hobby = "Singing";
+
+// display the object
+console.log(person);
+
+// Output: { name: 'Bobby', hobby: 'Singing' }
