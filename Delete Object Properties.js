f25a6346053bc6fb542d0baa8a459890854b286c
sricloudshare360 <sri.cloudshare360@gmail.com>
2024-11-12 00:26:30 +0530

    Create Delete Object Properties.js

---
+++ Delete Object Properties.js
@@ -0,0 +1,13 @@
+const employee = {
+    name: "Tony",
+    position: "Officer",
+    salary: 30000,
+};
+
+// delete object property
+delete employee.salary
+
+// display the object
+console.log(employee);
+
+// Output: { name: 'Tony', position: 'Officer' }
